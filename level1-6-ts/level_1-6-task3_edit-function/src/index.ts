7108c6122e0550cefe359c57478235be72aca7e5
KravchenkoMaks <maviegma@gmail.com>
2025-09-24 22:22:29 +0300

    change logic

--- level1-6-ts/level_1-6-task3_edit-function/src/index.ts
+++ level1-6-ts/level_1-6-task3_edit-function/src/index.ts
@@ -1,25 +1,23 @@
 interface BigObject {
-    [key: string]: Record<'value', number | string | BigObject | undefined>;
+    [key: string]: Record<'value', number | string | BigObject | undefined> | undefined;
 }
 
 function summ(a: BigObject): number {
-    const x: number[] = Object.keys(a).map(k => {
-        const elem: Record<'value', number | string | BigObject | undefined> = a[k];
-        if (elem.value == undefined) {
-            return elem.value === undefined ? 2021 : 0;
-        }
+    const x = Object.keys(a).map(k => {
+        const elem = a[k];
+        if (elem === undefined) return 2021;
         if (typeof elem.value === 'string') {
-            return isNaN(+elem.value) ? 2021 : +elem.value;
+            return isNaN(+elem.value) ? 2021 : (+elem.value);
         }
-        if (typeof elem.value === 'object') {
+        if (typeof elem.value == 'object' && elem.value !==  undefined) {
             return summ(elem.value);
         }
-        return elem.value;
+        return elem.value ;
     });
 
     let sum = 0;
     for (let i = 0; i < x.length; i++) {
-        sum += x[i];
+        sum +=  x[i] ?? 2021;
     }
     return sum;
 }
